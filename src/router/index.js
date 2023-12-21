0c218489ff1b4a52d9e2630d364a0337edfa0cd1
Rasith Upamal <rasithupamal@gmail.com>
2023-12-21 15:37:45 +0530

    router prevent with auth.

--- src/router/index.js
+++ src/router/index.js
@@ -10,36 +10,75 @@ const router = createRouter({
     {
       path: '/',
       name: 'task.list',
-      component: TaskList
+      component: TaskList,
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/tasks/create',
       name: 'task.create',
-      component: NewTask
+      component: NewTask,
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/tasks/:id/edit',
       name: 'task.update',
       component: UpdateTask,
-      props: true
+      props: true,
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/tasks/:id/show',
       name: 'task.show',
       component: ViewTask,
-      props: true
+      props: true,
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/login',
       name: 'auth.login',
-      component: () => import('../views/pages/auth/LoginPage.vue')
+      component: () => import('../views/pages/auth/LoginPage.vue'),
+      meta: {
+        guest: true
+      },
     },
     {
       path: '/register',
       name: 'auth.register',
-      component: () => import('../views/pages/auth/RegisterPage.vue')
+      component: () => import('../views/pages/auth/RegisterPage.vue'),
+      meta: {
+        guest: true
+      },
     }
   ]
+})
+
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (localStorage.getItem('token') == null) {
+      next({
+        name: 'auth.login'
+      })
+    } else {
+      next()
+    }
+  } else if (to.matched.some((record) => record.meta.guest)) {
+    if (localStorage.getItem('token') == null) {
+      next()
+    } else {
+      next({ name: 'task.list' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
